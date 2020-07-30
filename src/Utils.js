74f4f3f808df1fee0e62fa9dddbd0240ed677b56
DevinKott <dak3386@rit.edu>
2020-07-29 20:53:56 -0400

    change to named exports instead of default. writ saveText function that saves text to a file on the client

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,10 @@
-export default function isValid(obj) {
+import filesaver from 'file-saver'
+
+export function isValid(obj) {
     return obj !== null && obj !== undefined;
+}
+
+export function saveText(text, file) {
+    const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+    filesaver.saveAs(blob, file)
 }
